428758f8a4ca405d80440616d6ef2e097b8d8a71
Avi <avinash03082003@gmail.com>
2023-09-03 02:01:34 +0530

    Added Sep 03 POTD

---
+++ SEP_03_2023_Check_If_Tree_Is_Isomorphic_Solution.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h.
+using namespace std;
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+class Solution{
+  public:
+    // Return True if the given trees are isomotphic. Else return False.
+    bool isIsomorphic(Node *root1,Node *root2)
+    {
+    //add code here.
+        if(!root1 && !root2) return true;
+        if((root1 && !root2) || (!root1 && root2)) return false;
+        if(root1->data != root2->data) return false;
+        if((isIsomorphic(root1->left,root2->left) && isIsomorphic(root1->right,root2->right)) || (isIsomorphic(root1->right,root2->left) && (isIsomorphic(root1->left,root2->right)))) return true;
+        else return false;
+    }
+};
