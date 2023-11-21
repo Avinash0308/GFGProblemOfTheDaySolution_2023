5088e50560ff0e89565a982d62ee5c98c85bfef3
Avinash Agrawal <115834175+Avinash0308@users.noreply.github.com>
2023-11-21 12:28:55 +0530

    Create NOV_21_2023_Determine_If_Two_Trees_Are_Identical_Solution.cpp

---
+++ NOV_21_2023_Determine_If_Two_Trees_Are_Identical_Solution.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+
+   
+class Solution
+{
+    public:
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        //Your Code here
+        if(r1 == NULL && r2 == NULL) return true;
+        if(r1 != NULL && r2 == NULL || r2 != NULL && r1 == NULL) return false;
+        if(r1->data != r2->data) return false;
+        if(!isIdentical(r1->left,r2->left) || !isIdentical(r1->right,r2->right)) return false;
+        return true;
+    }
+};
