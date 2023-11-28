9c0b75c98b84ddd1058b3ab77958a033ac7eb845
Avinash Agrawal <avinash03082003@gmail.com>
2023-11-28 22:50:52 +0530

    Added Nov 28 POTD

---
+++ NOV_28_2023_Sum_Of_Dependencies_In_A_Graph_Solution.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int sumOfDependencies(vector<int> adj[], int V) {
+        // code here
+        int ans = 0;
+        for(int i = 0; i<V; i++){
+            ans += adj[i].size();
+        }
+        return ans;
+    }
+};
